86a5031814f6ab526023f6c4fe3998d38400c7b7
brachCamilo <ca.zamoras@duoocuc.cl>
2023-06-23 18:27:05 -0400

    formulario de contacto funcional con bd listo, pero sin estilo

--- libreria/urls.py
+++ libreria/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tienda/', include('tienda.urls')),
+    path('', include('tienda.urls')),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from .forms import ContactoForm
 # Create your views here.
+
+def contacto(request):
+    data = {
+        'form': ContactoForm()
+    }
+    if request.method == 'POST':
+        formulario = ContactoForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            data["mensaje"] = "Contacto Enviado"
+        else:
+            data["form"] = formulario
+
+    return render(request, 'tienda/contacto.html',data)
+
 def Inicio(request):
     context={}
     return render(request, 'tienda/Inicio.html', context)
@@ -19,9 +34,3 @@ def productos(request):
 
 def Registro(request):
     return render(request, 'tienda/Registro.html')
-
-def contacto(request):
-    data = {
-        'form': ContactoForm()
-    }
-    return render(request, 'tienda/contacto.html',data)
