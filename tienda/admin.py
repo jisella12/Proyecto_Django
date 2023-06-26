5c738814a6ee809ed6e22fd6f90be7e83eae76c2
jisella12 <jis.vergara@duocuc.cl>
2023-06-26 17:27:05 -0400

    holi

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+
+from tienda.forms import LibroForm
 from .models import Autor, Libro, Contacto
+
+
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "precio", "nuevo" "autor"]
+    list_editable = ["precio"]
+    search_fields = ["nombre"]
+    list_filter = ["autor", "nuevo"]
+    list_per_page = 5
+    form = LibroForm
+
+
+
 # Register your models here.
 admin.site.register(Autor)
 admin.site.register(Libro)
--- tienda/forms.py
+++ tienda/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Contacto, Libro
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .validator import MaxSizeFileValidator
+from .validator import ValidationError 
 
 class ContactoForm(forms.ModelForm):
     #validaciones
@@ -19,7 +21,16 @@ class LibroForm(forms.ModelForm):
     #validaciones
     nombre = forms.CharField(min_length=3, max_length=30)
     isbn = forms.IntegerField(min_value=2, max_value=400000)
-    imagen = forms.ImageField(required=False)
+    imagen = forms.ImageField(required=False, validators=[MaxSizeFileValidator(max_file_size=2)])
+    precio = forms.IntegerField(min_value=1, max_value=1500000)
+
+def clena_nombre(self):
+    nombre = self.cleaned_data["nombre"]
+    existe = Libro.objects.filter(nombre__iexact=nombre).exists()
+    if existe:
+        raise ValidationError("Este nombre Existe")
+    else:
+        return nombre 
   
     class Meta:
         model = Libro
